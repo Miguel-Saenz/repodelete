92c81f8a67a4e705bffa8235992fcaab286cde80
Miguel Dev <saenzm963@gmail.com>
2025-04-22 20:28:33 -0500

    Actualizar saludo.java

--- saludo.java
+++ saludo.java
@@ -4,4 +4,5 @@ public static void main (String []args) {
   JOptionPane.showMessageDialog(null, "Hola, este es un saludo", "SALUDO", 1);
   JOptionPane.showMessageDialog(null, "Mi nombre es XXXX");
 }
+//saludo
   
